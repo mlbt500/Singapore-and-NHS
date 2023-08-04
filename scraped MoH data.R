bfcd4fa327be62892da5ba8e2da100badc16d214
Max Thilo <129679995+mlbt500@users.noreply.github.com>
2023-08-05 01:05:41 +0800

    updated

--- scraped MoH data.R
+++ scraped MoH data.R
@@ -1,6 +1,7 @@
+library(rvest)
 url <- "https://www.moh.gov.sg/resources-statistics/singapore-health-facts/admissions-and-outpatient-attendances"
 SG <- read_html(url)
 table <- SG %>%
   html_table(fill = TRUE) %>%
   .[[1]] # Select the first table
-print(table, n=170)
+print(table)
