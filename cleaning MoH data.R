c189c19dc5b7e374b03f07b3e8e1f698e1e9f507
Max Thilo <129679995+mlbt500@users.noreply.github.com>
2023-08-15 20:40:48 +0100

    complete

--- cleaning MoH data.R
+++ cleaning MoH data.R
@@ -1,6 +1,14 @@
 library(dplyr)
 library(stringr)
 library(tidyr)
+library(WDI)
+library(rvest)
+
+url <- "https://www.moh.gov.sg/resources-statistics/singapore-health-facts/admissions-and-outpatient-attendances"
+SG <- read_html(url)
+table <- SG %>%
+  html_table(fill = TRUE) %>%
+  .[[1]]
 
 #name columns
 names(table) <- c("Values", "2018", "2019", "2020")
@@ -36,3 +44,20 @@ table2 <- table1
 table2[13:16, 2:4] <- table2[13:16, 2:4] * 1000
 table2 <- table2 %>%
   mutate(Values = str_extract(Values, "^[^\\(]+"))
+
+#final estimates
+GP_estimate <- data.frame(table2[15,])
+total_gp_estimate_2019 <- as.numeric(sum(GP_estimate[, 3] * 4))
+GP_estimate[, 2:4] <- GP_estimate[, 2:4] * 3
+GP_estimate[1, 1] <- "Private GP estimate"
+colnames(GP_estimate) <- names(table2)
+Singapore_health_utilisation <- rbind(table2, GP_estimate)
+hospital_admissions_SG <- as.numeric(sum(Singapore_health_utilisation[1:3,2]))
+
+#population and per capita
+Population <- WDI(indicator = "SP.POP.TOTL", country = "SG", start = 2019, end = 2019)
+Population_SG <- as.numeric(Population[,5])
+
+GP_visits_per_capita <- total_gp_estimate_2019/Population_SG
+hospital_admissions_per_capita <- hospital_admissions_SG/Population_SG
+
