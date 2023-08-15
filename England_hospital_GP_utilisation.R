5fed2e6b73d0bcfd1db3cf12204855720bff2e4b
Max Thilo <129679995+mlbt500@users.noreply.github.com>
2023-08-15 18:14:56 +0100

    completed

--- England_hospital_GP_utilisation.R
+++ England_hospital_GP_utilisation.R
@@ -1,7 +1,7 @@
 library(httr)
 library(readxl)
 library("openxlsx")
-
+library(WDI)
 #NHS England hospital admissions
 
 # URL of the file
@@ -36,12 +36,15 @@ if (http_status(download_status)$category == "Success") {
 }
 
 # Read the fourth sheet into the GP_appointments variable
-gp_appointments <- read_xlsx(output_file_gp, sheet = 4)
-gp_appointments <- gp_appointments[-c(1:4),]
-gp_appointments
-warnings()
+gp_appointments <- read_xlsx(output_file_gp, sheet = 4, col_names = FALSE, col_types = "text")
+gp_appointments <- gp_appointments[-c(1:3),]
+gp_appointments[1,2] <- NA
+unique(gp_appointments[1,])
+date_values <- as.Date(as.numeric(gp_appointments[1,]), origin = "1899-12-30")
+names(gp_appointments) <- date_values
+gp <- appointments <- gp_appointments[-1,]
 
-#clean hospital admissions
+#hospital admissions
 
 hospital_admissions_cleaned <- hospital_admissions[-c(1:2),]
 names <- hospital_admissions_cleaned[1,]
@@ -51,9 +54,22 @@ hospital_admissions_cleaned <- as.data.frame(hospital_admissions_cleaned)
 hospital_admissions_cleaned[,2] <- as.numeric(hospital_admissions_cleaned[, 2])
 hospital_admissions_cleaned[,2] <- as.numeric(hospital_admissions_cleaned[, 3])
 hospital_admissions_cleaned <- hospital_admissions_cleaned[-c(22:31),]
-no.hospital_admissions
+hospital_admissions_cleaned[, 2:3] <- lapply(hospital_admissions_cleaned[, 2:3], function(col) as.numeric(as.character(col)))
+adm_2018.2019 <- hospital_admissions_cleaned[21,3]
 
 #clean GP appointments
-GP_appointments
-GP_appointments_cleaned <- GP_appointments[GP_appointments[,1] == "Estimated England total count of appointments",2]
-no.GP_appoinments <- GP_appointments_cleaned[7,] # it outputs 7 rows for some reason but 24160000 is the correct figure
+gp_appointments <- read_xlsx(output_file_gp, sheet = 4, col_names = FALSE, col_types = "text")
+gp_appointments <- gp_appointments[-c(1:3),]
+gp_appointments[1,2] <- NA
+date_values <- as.Date(as.numeric(gp_appointments[1,]), origin = "1899-12-30")
+names(gp_appointments) <- date_values
+gp_appointments <- gp_appointments[-c(1:2),]
+gp_appointments[, 3:20] <- lapply(gp_appointments[, 3:20], function(col) as.numeric(as.character(col)))
+gp_appointments
+total_no_gp_app_2019 <- sum(gp_appointments[11,3:14])
+
+#population of England 2019
+England_population_2019 <- 56.3 * 10^6 # source ONS:https://www.ons.gov.uk/peoplepopulationandcommunity/populationandmigration/populationestimates/articles/overviewoftheukpopulation/january2021
+England_GP_app_per_capita_2019 <- total_no_gp_app_2019/England_population_2019
+England_hos_visits_per_capita <- adm_2018.2019/England_population_2019
+England_hos_visits_per_capita
