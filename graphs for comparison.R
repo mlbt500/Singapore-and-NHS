f1155c546e2eeb9d663e1a1354c97df7e7263aa1
Max Thilo <129679995+mlbt500@users.noreply.github.com>
2023-08-16 23:33:54 +0100

    completed graphs
    I need to make sure that the years covered for Singapore equals that for the UK.

--- graphs for comparison.R
+++ graphs for comparison.R
@@ -1,7 +1,56 @@
 unique(NTA3$Variable.Name)
 colnames(NTA3)
-#All the year values are 2013
-#The currency conversion for USD to Singaporean dollars is 1.5647, the currency conversion from USD to GBP is 0.7993
+library(ggplot2)
+
 unique(NTA3$Variable.Name)
 unique(NTA3$Country)
 unique(NTA3$Year)
+unique(NTA3$Age_Group)
+
+# 1. Filter out the rows for the three mentioned variables.
+subset_data <- NTA3[NTA3$Variable.Name %in% c("Public Consumption, Health", "Private Consumption, Health", "Public and Private, Health"),]
+
+# 2. Duplicate these rows
+new_data <- subset_data
+
+# 3. Modify the Value column based on the country's conversion rate
+new_data$Value[new_data$Country == "Singapore"] <- new_data$Value[new_data$Country == "Singapore"]/1.5647
+new_data$Value[new_data$Country == "United Kingdom"] <- new_data$Value[new_data$Country == "United Kingdom"]/0.7993
+
+# 4. Append "USD" to the Variable.Name column
+new_data$Variable.Name <- paste(new_data$Variable.Name, "USD")
+
+# Singapore 
+new_data$Age_Group <- gsub(pattern = "Age", replacement = "", x = new_data$Age_Group)
+new_data$Age_Group <- as.numeric(new_data$Age_Group)
+new_data_SG <- new_data[new_data$Country == "Singapore",]
+new_data_UK <- new_data[new_data$Country == "United Kingdom",]
+new_data_total <- new_data[new_data$Variable.Name == "Public and Private, Health USD", ]
+
+ggplot(new_data_SG, aes(x = Age_Group, y = Value, color = Variable.Name, group = Variable.Name)) +
+  geom_line(size = 1) + # this creates the lines
+  labs(
+    title = "Health Expenditures in Singapore (2013)",
+    x = "Age Group",
+    y = "Value (USD)",
+    color = "Expenditure Type"
+  ) +
+  theme_minimal() + # gives a clean theme
+  theme(legend.position="top") # puts the legend at the top
+
+
+ggplot(new_data_UK, aes(x = Age_Group, y = Value, color = Variable.Name)) +
+  geom_line() +
+  labs(title = "Health Expenditure in the United Kingdom (2013)",
+       x = "Age",
+       y = "Expenditure (USD)",
+       color = "Expenditure Type") +
+  theme_minimal()
+
+ggplot(new_data_total, aes(x = Age_Group, y = Value, color = Country)) +
+  geom_line() +
+  labs(title = "Total Health Expenditure (Public and Private) for Singapore and UK (2013)",
+       x = "Age",
+       y = "Expenditure (USD)",
+       color = "Country") +
+  theme_minimal()
--- health_spending_per_age_group.R
+++ health_spending_per_age_group.R
@@ -2,6 +2,7 @@ library(dplyr)
 library(tidyverse)
 library(WDI)
 library(knitr)
+library(ggplot2)
 library(kableExtra)
 
 # Reading CSV
@@ -168,4 +169,54 @@ final_html <- paste0(
 
 # Save the combined HTML to a file
 writeLines(final_html, "summary_table_with_key.html")
+
+# graphs for comparison
+
+# 1. Filter out the rows for the three mentioned variables.
+subset_data <- NTA3[NTA3$Variable.Name %in% c("Public Consumption, Health", "Private Consumption, Health", "Public and Private, Health"),]
+
+# 2. Duplicate these rows
+new_data <- subset_data
+
+# 3. Modify the Value column based on the country's conversion rate
+new_data$Value[new_data$Country == "Singapore"] <- new_data$Value[new_data$Country == "Singapore"]/1.5647
+new_data$Value[new_data$Country == "United Kingdom"] <- new_data$Value[new_data$Country == "United Kingdom"]/0.7993
+
+# 4. Append "USD" to the Variable.Name column
+new_data$Variable.Name <- paste(new_data$Variable.Name, "USD")
+
+# Singapore 
+new_data$Age_Group <- gsub(pattern = "Age", replacement = "", x = new_data$Age_Group)
+new_data$Age_Group <- as.numeric(new_data$Age_Group)
+new_data_SG <- new_data[new_data$Country == "Singapore",]
+new_data_UK <- new_data[new_data$Country == "United Kingdom",]
+new_data_total <- new_data[new_data$Variable.Name == "Public and Private, Health USD", ]
+
+ggplot(new_data_SG, aes(x = Age_Group, y = Value, color = Variable.Name, group = Variable.Name)) +
+  geom_line(size = 1) + # this creates the lines
+  labs(
+    title = "Health Expenditures in Singapore (2013)",
+    x = "Age Group",
+    y = "Value (USD)",
+    color = "Expenditure Type"
+  ) +
+  theme_minimal() + # gives a clean theme
+  theme(legend.position="top") # puts the legend at the top
+
+
+ggplot(new_data_UK, aes(x = Age_Group, y = Value, color = Variable.Name)) +
+  geom_line() +
+  labs(title = "Health Expenditure in the United Kingdom (2013)",
+       x = "Age",
+       y = "Expenditure (USD)",
+       color = "Expenditure Type") +
+  theme_minimal()
+
+ggplot(new_data_total, aes(x = Age_Group, y = Value, color = Country)) +
+  geom_line() +
+  labs(title = "Total Health Expenditure (Public and Private) for Singapore and UK (2013)",
+       x = "Age",
+       y = "Expenditure (USD)",
+       color = "Country") +
+  theme_minimal()
 
