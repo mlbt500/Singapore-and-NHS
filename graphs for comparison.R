013badf33d3a12de46714f49cbd3689f1391a872
Max Thilo <129679995+mlbt500@users.noreply.github.com>
2023-08-16 09:26:03 +0100

    sorted

---
+++ graphs for comparison.R
@@ -0,0 +1,7 @@
+unique(NTA3$Variable.Name)
+colnames(NTA3)
+#All the year values are 2013
+#The currency conversion for USD to Singaporean dollars is 1.5647, the currency conversion from USD to GBP is 0.7993
+unique(NTA3$Variable.Name)
+unique(NTA3$Country)
+unique(NTA3$Year)
