9b5de261a06cb59d27d24779058aba35a33adc23
Max Thilo <129679995+mlbt500@users.noreply.github.com>
2023-08-19 15:17:20 +0100

    Completed

--- DRG comparison.R
+++ DRG comparison.R
@@ -8,6 +8,8 @@ library(kableExtra)
 # values
 
 NEP_AUD_2018.19 <- 5012
+AUD_to_USD_2018 <- 0.7475
+SGD_to_USD_2019 <- 0.7331
 
 # Specify the URL for the Excel file -- MoH
 url <- "https://www.moh.gov.sg/docs/librariesprovider5/default-document-library/fee-publication-data-july18---june19-(for-download)_25-feb-2020.xlsx" 
@@ -28,9 +30,10 @@ S_data_sheet[3,]
 S_data_sheet <- S_data_sheet[-c(1:3),]
 colnames(S_data_sheet) <- names
 unique(S_data_sheet$`Ward Type`)
-Ward_A_S <- S_data_sheet[S_data_sheet$`Ward Type`== "Ward A",]
-Ward_A_S <- Ward_A_S[,c("DRG", "DRG Description", "P50 Bill")]
-colnames(Ward_A_S)
+Ward_A_SG <- S_data_sheet[S_data_sheet$`Ward Type`== "Ward A",]
+Ward_A_SG <- Ward_A_S[,c("DRG", "DRG Description", "P50 Bill")]
+colnames(Ward_A_SG)
+Ward_A_SG$"P50 Bill, USD 2019, July 2018-19" <- as.numeric(Ward_A_SG$"P50 Bill")*SGD_to_USD_2019
 
 # Optionally, you can remove the temporary file
 unlink(download_file)
@@ -41,5 +44,48 @@ names <- A_data_sheet[6,]
 A_data_sheet <- A_data_sheet[-c(1:6),]
 colnames(A_data_sheet) <- names
 AUS <- A_data_sheet[,c("product", "Cw", "Seps")]
+AUS$"Mean Unit Cost (AUD) 2018-19 FY" <- as.numeric(AUS$Cw)*NEP_AUD_2018.19
+AUS$"Mean Unit Cost (USD 2018, 18-19 FY)" <- AUS$"Mean Unit Cost (AUD) 2018-19 FY" * AUD_to_USD_2018
 
+#Compare Singapore and AUS
 
+# Extracting and transforming the relevant columns from AUS dataset
+AUS_selected <- AUS %>%
+  select(DRG = product,
+         `Unit Cost for Australia in USD` = `Mean Unit Cost (USD 2018, 18-19 FY)`,
+         `Seps AUS` = Seps)
+
+# Extracting and transforming the relevant columns from Ward_A_SG dataset
+SG_selected <- Ward_A_SG %>%
+  select(DRG, 
+         `DRG Description`,
+         `Unit Cost for Singapore in USD` = `P50 Bill, USD 2019, July 2018-19`)
+
+# Merging the two datasets on DRG ensuring only common DRGs are included
+final_table <- AUS_selected %>%
+  inner_join(SG_selected, by = "DRG") %>%
+  select(DRG, `DRG Description`, `Unit Cost for Australia in USD`, `Unit Cost for Singapore in USD`, `Seps AUS`)
+
+# Seps column coverted to numeric
+final_table$`Seps AUS` <- as.numeric(final_table$`Seps AUS`)
+
+# Adjust for Singaporean GST
+
+final_table$`Unit Cost for Singapore in USD` <- final_table$`Unit Cost for Singapore in USD`*1/1.07
+
+# Calculate the product of Unit Cost and Seps for Australia
+final_table$AUS_total_cost <- final_table$`Unit Cost for Australia in USD` * final_table$`Seps AUS`
+
+# Calculate the product of Unit Cost and Seps for Singapore
+final_table$SG_total_cost <- final_table$`Unit Cost for Singapore in USD` * final_table$`Seps AUS`
+
+# Calculate the weighted average unit cost for Australia
+AUS_weighted_avg_cost <- sum(final_table$AUS_total_cost) / sum(final_table$`Seps AUS`)
+
+# Calculate the weighted average unit cost for Singapore
+SG_weighted_avg_cost <- sum(final_table$SG_total_cost) / sum(final_table$`Seps AUS`)
+
+# Divide the weighted average unit cost of Singapore by that of Australia
+ratio <- SG_weighted_avg_cost / AUS_weighted_avg_cost
+
+ratio
