eaf5c8e9e08bfd25394539eb68ac8aac680bbf3d
Max Thilo <129679995+mlbt500@users.noreply.github.com>
2023-09-09 22:02:11 +0100

    completed

--- downloaded_directly_WDI.R
+++ downloaded_directly_WDI.R
@@ -29,38 +29,68 @@ y_min <- min(data_2019$health_care_per_capita_PPP, na.rm = TRUE) - 500
 y_max <- max(data_2019$health_care_per_capita_PPP, na.rm = TRUE) + 500
 
 # Plot 1: Health spending versus GDP per capita
-par(oma = c(3, 0, 0, 0))  # Adds space to the bottom outer margin
+# ... [your existing code above remains unchanged]
 
+# Adjust x-axis limits for both graphs
+x_max_gdp <- max(data_2019$GDP_per_capita_PPP, na.rm = TRUE) * 1.10
+x_min_gdp <- min(data_2019$GDP_per_capita_PPP, na.rm = TRUE) * 0.90
+
+x_max_consume <- max(data_2019$consumption_per_capita_PPP, na.rm = TRUE) * 1.10
+x_min_consume <- min(data_2019$consumption_per_capita_PPP, na.rm = TRUE) * 0.90
+
+# Exclude outliers for trend line calculations
+data_for_trend1 <- subset(data_2019, !(iso2c %in% c("SG", "IE", "US")))
+data_for_trend2 <- subset(data_2019, !(iso2c %in% c("SG")))
+
+# Set up side-by-side plotting
+par(mfrow=c(1,2), oma=c(4, 0, 0, 0))  # Sets up a 1x2 plotting area
+
+# Plot 1: Health spending versus GDP per capita
 plot(health_care_per_capita_PPP ~ GDP_per_capita_PPP, data = data_2019, 
-     xlab = "GDP per capita", 
-     ylab = "Health care spending per capita", 
-     main = "Health spending versus GDP per capita (PPP adjusted)",  
-     ylim = c(y_min, y_max))
+     xlim = c(x_min_gdp, x_max_gdp), ylim = c(y_min, y_max),
+     xlab = "GDP per capita", ylab = "Health care spending per capita", 
+     main = "", pch=16, col=ifelse(data_2019$iso2c %in% c("IE", "US", "SG", "GB"), "red", "gray"))
 
-offset <- (y_max - y_min) * 0.1  # Adjust offset for clarity
-text(data_2019$GDP_per_capita_PPP[data_2019$iso2c == "IE"], data_2019$health_care_per_capita_PPP[data_2019$iso2c == "IE"] - offset, labels = "IE")
-text(data_2019$GDP_per_capita_PPP[data_2019$iso2c == "US"], data_2019$health_care_per_capita_PPP[data_2019$iso2c == "US"] - offset, labels = "US")
-text(data_2019$GDP_per_capita_PPP[data_2019$iso2c == "SG"], data_2019$health_care_per_capita_PPP[data_2019$iso2c == "SG"] - offset, labels = "SG")
+# Adding trend line to the first graph (excluding outliers)
+model1 <- lm(health_care_per_capita_PPP ~ GDP_per_capita_PPP, data = data_for_trend1)
+abline(model1, lty = 2, lwd = 2)
 
-# Add footnote to the outer margin
-mtext("AU, CH, FI, FR, GB, IE, NL, SG, US, ES, GR, PT, IT, NO, TW, KR, JP", 
-      side = 1, line = 1, outer = TRUE, cex = 0.8)
+# Label countries for the first graph
+labels_to_add <- c("IE", "GB", "SG", "US")
+for(label in labels_to_add) {
+  text(data_2019$GDP_per_capita_PPP[data_2019$iso2c == label], 
+       data_2019$health_care_per_capita_PPP[data_2019$iso2c == label] - offset, 
+       labels = label, col = "red")
+}
 
 # Plot 2: Health spending versus Consumption per capita
 plot(health_care_per_capita_PPP ~ consumption_per_capita_PPP, data = data_2019,
-     xlab = "Consumption per capita", 
-     ylab = "Health care spending per capita",
-     main = "Health spending vs Consumption (Both PPP adjusted)")
+     xlim = c(x_min_consume, x_max_consume), ylim = c(y_min, y_max),
+     xlab = "Consumption per capita", ylab = "Health care spending per capita",
+     main = "", pch=16, col=ifelse(data_2019$iso2c %in% c("IE", "US", "SG", "GB"), "red", "gray"))
 
-# Calculate offset for text positioning
-offset <- (max(data_2019$health_care_per_capita_PPP) - min(data_2019$health_care_per_capita_PPP)) * 0.1
+# Adding trend line to the second graph (excluding outliers)
+model2 <- lm(health_care_per_capita_PPP ~ consumption_per_capita_PPP, data = data_for_trend2)
+abline(model2, lty = 2, lwd = 2)
 
-# Add text labels for select countries (US, GB, SG, and SG2)
-text(data_2019$consumption_per_capita_PPP[data_2019$iso2c == "US"], 
-     data_2019$health_care_per_capita_PPP[data_2019$iso2c == "US"] - offset, labels = "US")
-text(data_2019$consumption_per_capita_PPP[data_2019$iso2c == "GB"], 
-     data_2019$health_care_per_capita_PPP[data_2019$iso2c == "GB"] - offset, labels = "GBR")
-text(data_2019$consumption_per_capita_PPP[data_2019$country == "Singapore"], 
-     data_2019$health_care_per_capita_PPP[data_2019$country == "Singapore"] - offset, labels = "SG")
-text(data_2019$consumption_per_capita_PPP[data_2019$country == "SG2"], 
-     data_2019$health_care_per_capita_PPP[data_2019$country == "SG2"] - offset, labels = "SG2")
+# Label countries for the second graph
+for(label in labels_to_add) {
+  text(data_2019$consumption_per_capita_PPP[data_2019$iso2c == label], 
+       data_2019$health_care_per_capita_PPP[data_2019$iso2c == label] - offset, 
+       labels = label, col = "red")
+}
+
+
+# Add a central title for both graphs with the title spanning two lines
+title("Health Care Spending vs. Economic Indicators\n(2019 World Bank Development Indicators PPP adjusted)", outer = TRUE, line = -2)
+
+# Add footnotes to the outer margin
+
+# Add footnotes to the outer margin
+mtext("**AU, CH, FI, FR, GB, IE, NL, SG, US, ES, GR, PT, IT, NO, TW, KR, JP", 
+      side = 1, line = 2, outer = TRUE, cex = 0.8)
+mtext("*Trend lines exclude outliers (IE, SG and US in the first graph; SG in the second graph)", 
+      side = 1, line = 1, outer = TRUE, cex = 0.8)
+
+# Reset graphics parameters
+par(mfrow=c(1,1), oma=c(0,0,0,0))
