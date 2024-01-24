3d2c4b4694998e1e270277d2053146ac6ad36fc5
Max Thilo <129679995+mlbt500@users.noreply.github.com>
2024-01-24 09:27:17 +0000

    completed

--- out-of-pocket health expanded.R
+++ out-of-pocket health expanded.R
@@ -24,44 +24,38 @@ data_list$health_versus_consumption <- data_list$out_of_pocket_health_spend_USD_
 
 data_list$out_of_pocket_health_spend_pc_health_spend <- data_list$out_of_pocket_health_spend_pc_health_spend/100
 
-par(mfrow=c(1,2), oma=c(4, 0, 0, 0))  # Sets up a 1x2 plotting area
+# Set up a 1x2 plotting area with larger outer margins for title and footnote
+par(mfrow=c(1,2), oma=c(3, 0, 5, 0))  # Increase bottom and top outer margins
 
 # Set colors
 colors <- c("lightgray", "red", "black")
 
-# Create an empty plot
-plot(NULL, xlim=c(min(data_list$year), max(data_list$year)), ylim=c(min(data_list$out_of_pocket_health_spend_pc_health_spend, na.rm=TRUE), max(data_list$out_of_pocket_health_spend_pc_health_spend, na.rm=TRUE)),
-     xlab="Year", ylab="Out-of-Pocket Health/ Health Spend")
-
-# Loop through each country to draw lines
+# First Plot
+plot(NULL, xlim=c(min(data_list$year), max(data_list$year)), 
+     ylim=c(min(data_list$out_of_pocket_health_spend_pc_health_spend, na.rm=TRUE), 
+            max(data_list$out_of_pocket_health_spend_pc_health_spend, na.rm=TRUE)),
+     ylab="Out-of-Pocket Health/ Health Spend")
 for (country in unique(data_list$country)) {
   country_data <- data_list[data_list$country == country, ]
-  lines(country_data$year, country_data$out_of_pocket_health_spend_pc_health_spend, col=colors[which(unique(data_list$country) == country)], lwd=2)
+  lines(country_data$year, country_data$out_of_pocket_health_spend_pc_health_spend, 
+        col=colors[which(unique(data_list$country) == country)], lwd=2)
 }
 
-# Add legend
-legend("topright", legend=unique(data_list$country), col=colors, lty=1, lwd=2)
+# Add legend to the lower left in the margin under the first plot
+par(xpd=NA) # Allow plotting in the margin
+legend("bottomleft", inset=c(0.1, -0.6), legend=unique(data_list$country), col=colors, lty=1, lwd=2, horiz=TRUE)
 
-# Add footnote
-mtext("Source: World Bank Development Indicators", side=1, line=4, adj=0, cex=0.7)
-
-# Set colors
-colors <- c("lightgray", "red", "black")
-
-# Create an empty plot
-plot(NULL, xlim=c(min(data_list$year), max(data_list$year)), ylim=c(min(data_list$health_versus_consumption, na.rm=TRUE), max(data_list$health_versus_consumption, na.rm=TRUE)),
-     xlab="Year", ylab="Out-of-pocket health/ Consumption",
-     )
-
-# Loop through each country to draw lines
+# Second Plot
+plot(NULL, xlim=c(min(data_list$year), max(data_list$year)), 
+     ylim=c(min(data_list$health_versus_consumption, na.rm=TRUE), 
+            max(data_list$health_versus_consumption, na.rm=TRUE)),
+     xlab = "", ylab="Out-of-pocket health/ Consumption")
 for (country in unique(data_list$country)) {
   country_data <- data_list[data_list$country == country, ]
-  lines(country_data$year, country_data$health_versus_consumption, col=colors[which(unique(data_list$country) == country)], lwd=2)
+  lines(country_data$year, country_data$health_versus_consumption, 
+        col=colors[which(unique(data_list$country) == country)], lwd=2)
 }
 
-# Add legend
-legend("topright", legend=unique(data_list$country), col=colors, lty=1, lwd=2)
-
-# Add footnote
-mtext("Source: World Bank Development Indicators", side=1, line=4, adj=0, cex=0.7)
-
+# Add overall title and footnote
+mtext("Out-of-pocket health spend for \n Singapore, Switzerland, and Portugal", side= 3, line=1, outer=TRUE, cex=2)
+mtext("Source: World Bank Development Indicators", side=1, line=1, adj=0, cex=0.7, outer=TRUE)
