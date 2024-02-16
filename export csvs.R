860b77f37aacfdfe93de4c2abc11b3c935f5d9c3
Max Thilo <129679995+mlbt500@users.noreply.github.com>
2024-02-16 10:41:55 +0000

    Had to save and reload

---
+++ export csvs.R
@@ -0,0 +1,5 @@
+merged_data_birmingham <- merged_data[merged_data$ICB_NAME =="NHS Birmingham and Solihull ICB",]
+write.csv(merged_data_birmingham, "merged_data_birmingham.csv", row.names = FALSE)
+merged_data_birmingham$
+unique(merged_data$ICB_NAME)
+merged_data_birmingham <- merged_data[merged_data$ICB_NAME =="NHS Birmingham and Solihull ICB",]
